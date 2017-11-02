21aebebd1352f13faf6695b57008fc8f46e8b66b
Lafrance Banton <31461500+lbanton@users.noreply.github.com>
2017-11-02 17:23:20 -0400

    add location.js
    add locations

---
+++ CTUlocation.js
@@ -0,0 +1,38 @@
+               /*Location.js*/
+
+
+	          function Jamaica(){
+			  var message = "0. You’re on a ship in Montego Bay, off the Coast of Jamaica ready to venture through Paradise hoping to find some treasure.";
+			  }
+			  function Cuba(){
+			       var Message = "1. Welcome to Cuba don't forget to check out Havana for some cubain cegar.";
+			  }
+			  function Panama(){
+			       var message = "2. Welcome to Panama City please feel free to search the City, you may be lucky to find your fortune.";
+			  }
+			  function Cayman Islands(){
+				   var Message = "3. Welcome to the Cayman Islands, don't forget to check-in by our Treasure Island Resort.";
+			  }
+			  function Dominican Republic(){
+			  var  Message = "4. Welcome to Dominican Republic, there is lots of buried treasure off the shore of Punta Cana. Don't forget to bring you shovel and watch out for the sea shells.";
+			  }
+			  function Bahamas(){
+			  var   Message = "5. Welcome to the Bahamas.";
+			  }
+			  function Miami(){
+			  var  Message = "6. Welcome to Miami.";
+			  }
+			  function Turks and Caicos Islands(){
+			  var  Message = "7. Welcome to Turks and Caicos Islands.";
+			  }
+			  function Haiti(){
+			  var	 Message = "8. Welcome to Haiti.";  
+			  }
+			  function Aruba(){
+			  var  Message = "9. Welcome to Aruba.";
+			  }
+			  function Costa Rica(){
+			  var Message = "10. welcome to Costa Rica.";	  
+			  }
+			  
+			 
