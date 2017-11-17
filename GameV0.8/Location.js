12dd6fe3d65465918fa5cf6a867205d67b5c0430
Lafrance Banton <31461500+lbanton@users.noreply.github.com>
2017-11-16 22:33:23 -0500

    fix a few bugs

--- GameV0.8/Location.js
+++ GameV0.8/Location.js
@@ -21,11 +21,30 @@
    
          var locations = [];
    	     var Treasure = [];
-		 var items = [];
+		 var item = [];
 		 var inventory = [];
 		 
   // Store Locations in an array.
   
+  	 var Jamaica = new Location(0,"Jamaica","You’re on a ship in Montego Bay, off the Coast of Jamaica ready to venture through Paradise hoping to find some treasure.","Treasure-chest");
+	 var Cuba = new Location(1,"Cuba","Welcome to Cuba don't forget to check out Havana for some cubain cegar.","Cuban-cigar"); 
+	 var Panama = new Location(2,"Panama","Welcome to Panama City please feel free to search the City, you may be lucky to find your fortune.",);
+	 var Cayman = new Location(3,"Cayman","Welcome to the Cayman Islands, don't forget to check-in by our Treasure Island Resort.",);
+	 var DomincainRepublic = new Location(4,"Domincain-Republic",null);
+	 var Bahamas = new Location(5,"Bahamas","4Welcome to Dominican Republic, there is lots of buried treasure off the shore of Punta Cana. Don't forget to bring you shovel and watch out for the sea shells.",null);
+	 var Miami = new Location(6,"Miami","Magic-skull" );
+	 var TurksandCaicos = new Location(7,"Turks-and-Caicos",null);
+	 var Haiti = new Location(8,"Haiti","Welcome to Haiti.",null);
+	 var Aruba = new Location(9,"Aruba","Welcome to Aruba.","Rare-pearl");
+	 var CostaRica = new Location(10,"Costa-Rica","Welcome to Costa Rica.","Golden Compass");
+
+	var Treasurechest = new Item (0,"Treasurechest" , "Wow! look what you found" , "Treasure chest");
+	var Rarepearl = new Item (1, "Rare-pearl" , "sweet" , "Rare pearl");
+	var Magicskullkey = new Item (2,"Magic-skullkey","Awesome","Magic skull key");
+	var Cubancigar = new Item (3,"Cubancigar","dont smoke the hold thing at once you just find a ","Cubancigar");
+	var GoldenCompass = new Item(4,"GoldenCompass","You think your so cool","Golden Compass");
+    
+	 
      locations[0]= Jamaica;
 	 locations[1]= Cuba;
 	 locations[2]= Panama;
@@ -37,63 +56,55 @@
 	 locations[8]= Haiti; 
 	 locations[9]= Aruba;      
 	 locations[10]= CostaRica;
-	 Location.push(Jamaica, Cuba, Panama, Cayman, TurksandCaicos, Haiti, Aruba, CostaRica); 
 	 
   // item array 
      
-	 items[0] = Treasurechest;
+	 item[0] = Treasurechest;
 	 item[1] = Rarepearl;
-	 items[2] = Goldencompass;
-	 items[3] = Magicskullkey;
-	 items[4] = Cubancigar;
+	 item[2] = GoldenCompass;
+	 item[3] = Magicskullkey;
+	 item[4] = Cubancigar;
 	 
 	   //   Initialization 
     function init() {
-             look();   
-	}	 
+                
+	}
+    function look(){
+		console.log(currentLoc);
+		console.log(locations);
+		console.log(locations[currentLoc]);
+		var desc = locations[currentLoc].desc;
+		updatedisplayMessage(desc);
+	}
   //	 Add locations and items  prototype
   //     ("Treasure-chest","Rare-pearl","Golden-compass","Magic-skull-key","Cuban-cigar","")
   
-	 function locations(id, name, desc, item) {
-	 this.id = id;
-	 this.name = name;
-	 this.desc = desc;
-	 this.item = item; 
-	 locations.toString = function() {
-                                 return this.id + "" + this.name + "" + this.description + "" + this.item
-                            };
-	 var Jamaica = new locations(0,"Jamaica","You’re on a ship in Montego Bay, off the Coast of Jamaica ready to venture through Paradise hoping to find some treasure.","Treasure-chest");
-	 var Cuba = new locations(1,"Cuba","Welcome to Cuba don't forget to check out Havana for some cubain cegar.","Cuban-cigar"); 
-	 var Panama = new locations(2,"Panama","Welcome to Panama City please feel free to search the City, you may be lucky to find your fortune.",);
-	 var Cayman = new locations(3,"Cayman","Welcome to the Cayman Islands, don't forget to check-in by our Treasure Island Resort.",);
-	 var DomincainRepublic = new locations(4,"Domincain-Republic",null);
-	 var Bahamas = new locations(5,"Bahamas","4Welcome to Dominican Republic, there is lots of buried treasure off the shore of Punta Cana. Don't forget to bring you shovel and watch out for the sea shells.",null);
-	 var Miami = new locations(6,"Miami","Magic-skull" );
-	 var TurksanCaicos = new locations(7,"Turks-and-Caicos",null);
-	 var Haiti = new locations(8,"Haiti","Welcome to Haiti.",null);
-	 var Aruba = new locations(9,"Aruba","Welcome to Aruba.","Rare-pearl");
-	 var CostaRica = new locations(10,"Costa-Rica","Welcome to Costa Rica.","Golden Compass");
-	 }    
+	 function Location(id, name, desc, item) {
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+		this.item = item; 
+		this.toString = function() {
+			return this.id + "" + this.name + "" + this.description + "" + this.item;
+        };
+	 }						
+
+	    
 	 
-	function items (id, name, desc, item) {
-	 this.id = id;
-	 this.name = name;
-	 this.desc = desc;
-	 this.item = item;
-	 items.toString = function() {
-                                 return this.id + "" + this.name + "" + this.description + "" + this.item
+	function Item (id, name, desc, item) {
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+		this.item = item;
+		this.toString = function() {
+                                 return this.id + "" + this.name + "" + this.description + "" + this.item;
                             };
+	}
 	
-	
-	var Treasurechest new items (0,"Treasurechest" , "Wow! look what you found" , "Treasure chest");
-	var Rarepearl new items (1, "Rare-pearl" , "sweet" , "Rare pearl");
-	var Magicskullkey new items (2,"Magic-skullkey","Awesome","Magic skull key");
-	var Cubancigar new items (3,"Cubancigar","dont smoke the hold thing at once you just find a ","Cubancigar");
-	var GoldenCompass new items(4,"GoldenCompass","You think your so cool","Golden Compass");
-    }   
+
   //   Debug Aid	
    
-	     console.log(items[]);
+	     console.log(item);
 		 
 	function Novi_erroLoc() {
 		erroLoc = erroLoc + 1;
@@ -106,7 +117,7 @@
   //	}
  //     Event handler for each button for North, South, East, West BUG needs******* to be remove******
 	 function btnNorth_click(){
-	        if (locations[0] === 0){
+	        if (currentLoc === 0){
 			    currentLoc = 1;
 			    score += 5;
 				look ();
@@ -165,11 +176,8 @@
 	            return score;
 				}
              }				
-        } 
-		    
-            displayMessage(msg);
-			document.getElementById("Score").innerHTML = "Score:"+score;
-			
+        } 	
+		
 	//		cardinal direction
 	
 	function btnGo_click(){
@@ -186,9 +194,9 @@
 			                      updatedisplayMessage("Please Click on the navigation button below (North, South, East, West) or type in the following letters (N,S,E,W,n,s,e,w,) to navigate your ship!!!");
 		                        }
 			}	
-	function updatedisplayMessage(locations.toString ) {
+	function updatedisplayMessage(msg) {
              var target = document.getElementById("message");
-             target.value = locations.toString +"\n\n" + target.value;
+             target.value = msg +"\n\n" + target.value;
             } 
       //       Help button		 
     function btnhlp_click() {
