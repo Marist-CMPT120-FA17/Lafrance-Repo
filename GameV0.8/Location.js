ce0a52175ba4bc9a2177ffa8d5ef3c25d9209229
Lafrance Banton <31461500+lbanton@users.noreply.github.com>
2017-11-28 05:58:17 -0500

    fix unlimited score

--- GameV0.8/Location.js
+++ GameV0.8/Location.js
@@ -8,20 +8,22 @@
 	  var score = 0;
       var currentLoc = 0;
 	  
-	 // location matrix
-	 var nav = [// N S E W
-         /*0*/	  [1,2,8,4 ],
-		 /*1*/    [5,0,7,-1],
-		 /*2*/    [0,-1,10,9],
-		 /*3*/    [1,10,8,-1],
-		 /*4*/    [7,9,-1,8],
-		 /*5*/    [-1,1,7,6], 
-		 /*6*/    [-1,-1,5,-1], 
-		 /*7*/    [-1,4,-1,-1], 
-		 /*8*/    [7,9,4,0], 
-		 /*9*/    [4,-1,2,-1], 
-		 /*10*/   [3,-1,2,-1],
-                         ] ;	
+	 // locations Matrix
+	 
+	 var nav = [// N  S  E  W
+	 
+         /*0*/	  [1, 2, 8, 3 ],
+		 /*1*/    [5, 0, 7, -1],
+		 /*2*/    [0, -1, 10, 9],
+		 /*3*/    [1, 10, 8, -1],
+		 /*4*/    [7, 9, -1, 8],
+		 /*5*/    [-1, 1, 7, 6], 
+		 /*6*/    [-1, -1, 5, -1], 
+		 /*7*/    [-1, 4, -1,-1], 
+		 /*8*/    [7, 9, 4, 0], 
+		 /*9*/    [4, -1, 2, -1], 
+		 /*10*/   [3, -1, 2, -1],
+                ];	
 	  var VisitedLoc0 = false;
       var VisitedLoc1 = false;
       var VisitedLoc2 = false;
@@ -33,11 +35,7 @@
       var VisitedLoc8 = false;
       var VisitedLoc9 = false;
       var VisitedLoc10 = false;
-	  
-			
-					
-	  
-		 
+
    //Add Global array for look() and Inventory("Treasure")
    
       var locations =[];
@@ -52,7 +50,7 @@
 	  var Cayman = new Location(3,"Cayman","Welcome to the Cayman Islands, don't forget to check-in by our Treasure Island Resort.",null);
 	  var DomincainRepublic = new Location(4,"Domincain-Republic","Dominican Republic, there is lots of buried treasure off the shore of Punta Cana. Don't forget to bring you shovel and watch out for the looters.",null);
 	  var Bahamas = new Location(5,"Bahamas","Welcome to the Bahamas",null);
-	  var Miami = new Location(6,"Miami","welcome to Miami, would you like to update you ship","Magic Skull key" );
+	  var Miami = new Location(6,"Miami","welcome to Miami, would you like a ship upgrade","Magic Skull key" );
 	  var TurksandCaicos = new Location(7,"Turks-and-Caicos","missing text",null);
 	  var Haiti = new Location(8,"Haiti","Welcome to Haiti.","missing text",null);
 	  var Aruba = new Location(9,"Aruba","Welcome to Aruba. Sweet! it looks like you just found a.","Rare-pearl");
@@ -62,7 +60,6 @@
 	  var Cubancigar = new Item (1,"Cuban cigar","You just retrived a Cuban cigar!!!");
 	  var Rarepearl = new Item (3,"Rare pearl","Nice! You just retrived a Rare pearl!!!");
 	  var Magicskullkey = new Item (2,"Magic skull key","Awesome! You just retrived the Magic Skull key!!");
-	  
 	  var GoldenCompass = new Item(4,"Golden Compass","You just retrive the Golden Compass!!");
     
     locations[0]= Jamaica;
@@ -102,8 +99,8 @@
 		console.log(items);
 		console.log(score);
 		console.log(hiddenTresure);
-		console.log(items.length);
-		//console.log(btnNorth_click()); game crash when this console is active in the program
+		//console.log(item.length);
+		//console.log(btnNorth_click()); //game crash when this console is active in the program
 		console.log(btnitem_click);
 		console.log(Treasure);
 		console.log(displayinventorymsg);
@@ -132,42 +129,62 @@
                                  return this.id + "" + this.name + "" + this.desc;
                         };
 	}
-  //   Debug Aid	
-   
-	     console.log();
-		 
-	         function nextLoc(dir) {
-            var newLoc = nav[currentLoc][dir];
-            if (newLoc >= 0) {
-               currentLoc = newLoc;
+	
+  //  This function Works with the Navigation matrix and add score by 5 points 
+  
+	function nextLoc(dir) {
+		var newLoc = nav[currentLoc][dir];
+        if (newLoc >= 0) {
+            currentLoc = newLoc;
+		    score +=5;
             } else {
                updatedisplayMessage("You cannot go that way.");
             }            
          }
+  //    check score function	
+    /* function chkscore(){
+		  var item = locations[currentLoc].item;
+		      if(item.length == 1){  
+	                  }else{
+					     if(item.length == 0){
+                            return score;
+						           }
+						    }   
+	 }*/  
+	 
+	/* function chkscore(){
+	 if([currentLoc] <=11){
+			 score +=5;
+		 }else{
+			 ([currentLoc] == 0 )
+				 return score;
+		      }
+	 }*/
   //    Event handler for each button for North, South, East, West.
   
 	 function btnNorth_click(){
 	          nextLoc(0);
 		      hiddenTresure = 1;
-			  score += 5;
+	//		  chkscore();
 			  look();			
     }
 	 function btnSouth_click(){
-              nextLoc(0);
+              nextLoc(1);
 			  hiddenTresure = 2;
-			  score += 5;
+	//		  chkscore();
 			  look();
             				
      }
 	 function btnEast_click(){
-		      nextLoc(0);
-			  hiddenTresure = 2;
+		      nextLoc(3);
+			  hiddenTresure = 3;
+	//		  chkscore();
 			  look();
 	 }
 	 function btnWest_click(){
-		      nextLoc(0);
-		      hiddenTresure = 6
-			  score += 5;
+		      nextLoc(4);
+		      hiddenTresure = 4
+	//	  chkscore();
 			  look();
       }
 		// this btnitem_click() function add treatures to inventory when found
@@ -199,8 +216,9 @@
 							displayinventorymsg(Treasure);
 							updatedisplayMessage(name);
 						}else{
+							if(hiddentresure == null); 
 							return Treasure;
-							displayinventorymsg("Sorry there is no Treasure");
+							updatedisplayMessage("Sorry there is no Treasure here");
 			            }
 					}
 		        }
